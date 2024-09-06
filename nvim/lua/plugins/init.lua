693713af8df2745ed0ed03e2f50f81a2c8857d58
Jack Hedaya <jackehedaya@gmail.com>
2024-09-06 10:59:47 -0400

    wezterm nvim

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "ray-x/go.nvim",
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+      "mfussenegger/nvim-dap",
+    },
+    config = function ()
+      require("go").setup({
+        lsp_cfg = true,
+        lsp_gofumpt = true,
+        dap_debug = true,
+        test_runner = 'richgo'
+      })
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", "gomod"},
+    build = ':lua require("go.install").update_all_sync()'
+  }
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
